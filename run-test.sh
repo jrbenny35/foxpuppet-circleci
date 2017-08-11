36560397f6a3e48ff6c09663c42db2dd8c4f46e0
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-08-11 10:58:45 -0700

    Add everything to one script

---
+++ run-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+IS_PONTOON=$(git show -s --format=%s | grep -q 'Pontoon:' && echo 'true' || echo '')
+if [[ $IS_PONTOON ]]; then
+  echo "Skipping Integration Tests on Pontoon commit.";
+  exit 0;
+fi
+
+GECKODRIVER_URL=$(
+  curl -s 'https://api.github.com/repos/mozilla/geckodriver/releases/latest' |
+  python -c "import sys, json; r = json.load(sys.stdin); print([a for a in r['assets'] if 'linux64' in a['name']][0]['browser_download_url']);"
+);
+
+curl -L -o geckodriver.tar.gz $GECKODRIVER_URL
+gunzip -c geckodriver.tar.gz | tar xopf -
+chmod +x geckodriver
+sudo mv geckodriver /home/ubuntu/bin
+geckodriver --version
+
+pip install tox mozdownload mozinstall
+
+mkdir -p /home/ubuntu/firefox-downloads/
+mozdownload --version latest --type daily --destination /home/ubuntu/firefox-downloads/firefox_nightly/
+mozinstall $(ls -t /home/ubuntu/firefox-downloads/firefox_nightly/*.tar.bz2 | head -1)
+firefox --version
+cd FoxPuppet
+mkdir results
+tox
